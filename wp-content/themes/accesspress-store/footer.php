23199075fcebfcde0462c432c9a09e3620189203
checkomkar <check.omkar@gmail.com>
2016-11-01 22:46:37 +0530

    Plugins and analytics

--- wp-content/themes/accesspress-store/footer.php
+++ wp-content/themes/accesspress-store/footer.php
@@ -96,7 +96,16 @@
         </div><!-- .site-info -->
     </div>
 </div>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-86655284-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </footer><!-- #colophon -->
 </div><!-- #page -->
 <div id="ak-top"><i class="fa fa-reply"></i></div>
